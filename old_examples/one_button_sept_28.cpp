563193ad3eed26977e97da888e4e3e4ccc16e867
jawinikus <jwinikus@buffalo.edu>
2022-09-30 21:48:31 +0000

    Sept 30
    Signed-off-by: jawinikus <jwinikus@buffalo.edu>

--- old_examples/one_button_sept_28.cpp
+++ old_examples/one_button_sept_28.cpp
@@ -1,26 +1,26 @@
-//Sept 28
-#include "mbed.h"
+// //Sept 28
+// #include "mbed.h"
 
-int main(){
-    RCC->AHB2ENR|=0x4;
-    //want 10000
-    GPIOC->MODER|=0x10000; //
-    GPIOC->MODER&=~(0xE0000); //
-   RCC->AHB2ENR|=0x2;
+// int main(){
+//     RCC->AHB2ENR|=0x4;
+//     //want 10000
+//     GPIOC->MODER|=0x10000; //
+//     GPIOC->MODER&=~(0xE0000); //
+//    RCC->AHB2ENR|=0x2;
 
-  // 14 and 15 on MODER
-  // 01 val
-  GPIOB->MODER |= 0x4000;
-  GPIOB->MODER &=~(0x8000);
-GPIOB->MODER=0x4000;
-      while(1){
-          GPIOC->ODR|=0x100;
-      if((GPIOC->IDR&0x200)==0x200){ //pc9
-        GPIOB->ODR |=(0x80);
-      }
-        else{
-GPIOB->ODR&=~(0x80);
-}
-  }//new piece
-  return 0;
-}
+//   // 14 and 15 on MODER
+//   // 01 val
+//   GPIOB->MODER |= 0x4000;
+//   GPIOB->MODER &=~(0x8000);
+// GPIOB->MODER=0x4000;
+//       while(1){
+//           GPIOC->ODR|=0x100;
+//       if((GPIOC->IDR&0x200)==0x200){ //pc9
+//         GPIOB->ODR |=(0x80);
+//       }
+//         else{
+// GPIOB->ODR&=~(0x80);
+// }
+//   }//new piece
+//   return 0;
+// }
---
+++ polling_ex1.cpp
@@ -0,0 +1,39 @@
+//sept 30
+#include "mbed.h"
+
+int main(){
+    //enable 
+//use port B for out -> rows
+//use c for in -> column
+RCC->AHB2ENR|=0x6;
+    //mode
+    //4 outs and 4 in
+    //8, 9, 10, 11
+    //00 in, 01 is out
+    //pins 16-23
+    
+    GPIOC->MODER&=~(0xFF0000);
+    GPIOB->MODER&=~(0xAA0000);
+    GPIOB->MODER|=0x550000;
+
+    //poll
+    //while loop
+while(1){
+    GPIOB->ODR=0x100;//turn on pin 8
+    if((GPIOC->IDR&0x100)==0x100){
+        printf("row 1 column 1");
+    }
+    else if((GPIOC->IDR&0x200)==0x200){
+        printf("row 1 column 2");
+    }
+    GPIOB->ODR=0x200;
+    if((GPIOC->IDR&0x100)==0x100){
+        printf("row 2 column 1");
+    }
+    else if((GPIOC->IDR&0x200)==0x200){
+        printf("row 2 column 2");
+    } 
+}
+
+    return 0;
+}
